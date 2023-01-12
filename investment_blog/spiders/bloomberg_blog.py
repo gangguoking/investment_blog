4612161881358b8bb18436bfeed9463aa391b466
huhao <huhao13053282925@gmail.com>
2023-01-12 10:09:52 +0800

    去除 IMAGE_HEADER 无效代码

--- investment_blog/spiders/bloomberg_blog.py
+++ investment_blog/spiders/bloomberg_blog.py
@@ -25,23 +25,6 @@ SEARCH_HEADER = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
 }
 
-IMAGE_HEADER = {
-    'authority': 'assets.bwbx.io',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-language': 'en,zh-CN;q=0.9,zh;q=0.8',
-    'cache-control': 'max-age=0',
-    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-
-}
-
 # 最大翻页数
 MAX_PAGE = 5
 
