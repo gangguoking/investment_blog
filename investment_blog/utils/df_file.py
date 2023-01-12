daff9459094501327547dac1730635a13dbdcb14
huhao <huhao13053282925@gmail.com>
2023-01-12 14:51:47 +0800

    更新 df_file.py

--- investment_blog/utils/df_file.py
+++ investment_blog/utils/df_file.py
@@ -9,10 +9,11 @@ def csv_to_excel(csv_filename, excel_filename):
     :return:
     """
     df = pd.read_csv(csv_filename)
-    # print(df.columns.to_list())
     df.drop_duplicates("url", inplace=True)
+    df = df.drop(df[df['authors'] == 'authors'].index)
     df.set_index(df.columns.to_list()[0], inplace=True)
     df.to_excel(excel_filename)
+    df.to_csv(csv_filename)
 
 
 if __name__ == '__main__':
